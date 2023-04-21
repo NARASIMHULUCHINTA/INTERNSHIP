b09b6bc8ffc717cf5333d2840838c9e7254c956e
NARASIMHULU CHINTA <87014074+NARASIMHULUCHINTA@users.noreply.github.com>
2023-04-21 21:03:26 +0530

    Add files via upload

---
+++ LEET CODE/21-04-2023/diameterofBinarytree.js
@@ -0,0 +1,12 @@
+var diameterOfBinaryTree = function (root) {
+    let diameter = 0;
+    dfs(root);
+    return diameter;
+    function dfs(node) {
+        if (!node) return 0;
+        const left = dfs(node.left);
+        const right = dfs(node.right);
+        diameter = Math.max(diameter, left + right);
+        return 1 + Math.max(left, right);
+    }
+}    
